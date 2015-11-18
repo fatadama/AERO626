Validated EnKF for basic problem. Updated algorithm to include random process noise in the propagation, which is theoretically necessary to maintain the correct covariance.

filters/python/enkf/enkf.py
@@ -2,39 +2,100 @@
 Ensemble Kalman Filter for systems with linear updates and (possibly) nonlinear propagation functions
 """
 
-import numpy as numpy
+import numpy as np
 import math
+import scipy.integrate as sp
 
 class enkf():
 	## __init__
 	#
 	# @param[in] n Length of state vector
 	# @param[in] m length of control vector
-	# @param[in] propagateFunction Function that returns the state derivative as follows: dx = propagateFunction(x,t,u)
+	# @param[in] propagateFunction Function that returns the state derivative as follows: dx = propagateFunction(x,t,u,v). u is the system control, v is the noise
 	# @param[in] Hk measurement function input matrix
 	# @param[in] Qk process noise covariance matrix
 	# @param[in] Rk measurement noise covariance matrix 
-	def __init__(self,n=1,m=1,propagateFunction=None,Hk=None,Qk=None,Rk=None):
+	# @param[in] Ns number of emsemble samples to use (fixed here)
+	def __init__(self,n=1,m=1,propagateFunction=None,Hk=None,Qk=None,Rk=None,Ns=None):
 		## state vector length
-		self_n = n
+		self._n = n
 		## control length
 		self._m = m
+		## control vector
+		self.u = np.zeros(m)
+		## current time
+		self.t = 0.0
 		## propagation function
 		self._propagateFunction = propagateFunction
 		self._Hk = Hk
 		self._Qk = Qk
 		self._Rk = Rk
 		## number of ensemble members (samples)
-		self._N = 2*n+1
+		if Ns is not None:
+			self._N = Ns
+		else:
+			self._N = 2*n+1
 		## initialization flag
 		self._initFlag = False
 		## ensemble state vector
-		self.xk = np.zeros((self._n,self._N)
+		self.xk = np.zeros((self._n,self._N))
+	## get_N get the number of ensemble members
+	#
+	# @param[out] No number of ensemble members
+	def get_N(self):
+		return self._N
+	## set_control set the control to a specified value
+	#
+	# @param[in] ui m-length numpy vector to set the control to
+	def set_control(self,ui):
+		self.u = ui.copy()
 	## init Initialize the ensemble estimates with the apriori mean and covariance
 	#
 	#	@param[in] mux n-length numpy vector
 	#	@param[in] Pxx n x n numpy array
 	def init(self,mux,Pxx):
-		self.xk = np.random.multivariate_normal(mux,Pxx,size=(self._n,))
-		print(self.xk.shape)
+		self.xk = np.random.multivariate_normal(mux,Pxx,size=(self._N,)).transpose()
 		return
+	## propagateOde propagate the system using the scipy integrator
+	def propagateOde(self,dt):
+		# generate process noise for each sample, size is nv x N where nv is length of process noise vector
+		vk = np.random.multivariate_normal(np.zeros(self._Qk.shape[0]), self._Qk, size=(self._N,)).transpose()
+		# propagate each sample
+		for k in range(self._N):
+			y = sp.odeint(self._propagateFunction,self.xk[:,k],np.array([self.t,self.t+dt]),args=(self.u,vk[:,k],))
+			self.xk[:,k] = y[-1,:].copy()
+		# update the time
+		self.t = self.t + dt
+	## propagate the system for dt seconds using a first-order Euler approximation
+	def propagate(self,dt):
+		# generate process noise for each sample, size is nv x N where nv is length of process noise vector
+		vk = np.random.multivariate_normal(np.zeros(self._Qk.shape[0]), self._Qk, size=(self._N,)).transpose()
+		# propagate each sample
+		for k in range(self._N):
+			self.xk[:,k] = self.xk[:,k] + dt*self._propagateFunction(self.xk[:,k],self.t,self.u,vk[:,k])
+		# update the time
+		self.t = self.t + dt
+		return
+	## update update the system in response to a measurement
+	#
+	# @param[in] ymeas measurement at current time
+	def update(self,ymeas):
+		# generate sample measurement noises from the covariance _Rk
+		# dy is N x h where h is the number of outputs
+		dy = np.random.multivariate_normal( np.zeros(self._Rk.shape[0]) ,self._Rk,size=(self._N,)).transpose()
+		# compute the predicted mean
+		xhatm = np.mean(self.xk,axis=1)
+		# compute the state covariance Pxx
+		coef = 1.0/(1.0+float(self._N))
+		Pxx = np.zeros((self._n,self._n))
+		for k in range(self._N):
+			Pxx = Pxx + coef*np.outer(self.xk[:,k]-xhatm,self.xk[:,k]-xhatm)
+		# inverse term for the Kalman gian
+		invTerm = np.linalg.inv(self._Rk + np.dot(self._Hk,np.dot(Pxx,self._Hk.transpose())))
+		# Compute the Kalman gain
+		Kk = np.dot(Pxx,np.dot(self._Hk.transpose(),invTerm))
+		# update each state
+		for k in range(self._N):
+			innov = Kk*(ymeas + dy[:,k] - np.dot(self._Hk,self.xk[:,k]))
+			for j in range(self._n):
+				self.xk[j,k] = self.xk[j,k] + innov[j]

filters/python/enkf/test_enkf.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 import scipy.integrate as sp
+import matplotlib.pyplot as plt
 
 import enkf
 
@@ -34,10 +35,16 @@ def stateDerivative(x,t,u):
     f[1] = -2.0*(1.5/1.0)*(x[0]*x[0]-1.0)*x[1]-(1.2/1.0)*x[0]
     return f
 
-def stateDerivativeEKF(x,t,u):
+## stateDerivativeEKF - function used by the filter for propagation
+#
+#   @param x state given by ( position(inertial), velocity(body frame), quaternion )
+#   @param t current time
+#   @param u control term, not used here
+#   @param vk process noise vector, used for filter
+def stateDerivativeEKF(x,t,u,vk):
     f = np.zeros((2,))
     f[0] = x[1]
-    f[1] = -2.0*(1.0/1.0)*(x[0]*x[0]-1.0)*x[1]-(1.0/1.0)*x[0]
+    f[1] = -2.0*(1.0/1.0)*(x[0]*x[0]-1.0)*x[1]-(1.0/1.0)*x[0] + vk[0]
     return f
 
 ## stateGradient(x,t,u) - returns the gradient of the derivative of the filter state w.r.t. the filter state
@@ -89,14 +96,11 @@ def main(argin='./'):
     FOUT.write('t,x1,x2,ymeas,x1hat,x2hat,P11,P22\n');
 
     # initialize EKF
-    Qkin = np.array([[20.0]])
-    #EKF = ekf.ekf(2,1,stateDerivativeEKF,stateGradient,stateProcessInfluence,Qk = Qkin)
+    Qkin = np.array([[20.0]])#continuous-time integration value
+    #Qkin = np.array([[20.0]])#Euler integration value
     Hkin = np.array([[1.0,0.0]])
     Rkin = np.array([ [0.0001] ])
-    EnFK = enkf.enkf(2,1,stateDerivativeEKF,Hk=Hkin,Qk=Qkin,Rk=Rkin)
-
-    return
-    """
+    EnKF = enkf.enkf(2,1,stateDerivativeEKF,Hk=Hkin,Qk=Qkin,Rk=Rkin,Ns=50)
 
     dt = 0.01
     tfin = 10.0
@@ -105,14 +109,26 @@ def main(argin='./'):
 
     xk = np.array([1.0,0.0])
     yk = measFunction(xk,tsim)
-    EKF.init(yk,initFunction,tsim)
 
-    print(nSteps)
+    # initial covariance
+    P0 = np.diag([Rkin[0,0],1.0])
+    EnKF.init(xk,P0)
 
+    Enkfx = np.mean(EnKF.xk,axis=1)
+
+    xt = np.zeros((nSteps,2))
+    xf = np.zeros((nSteps,2))
+    Pxd = np.zeros((nSteps,2))
+    Pxx = np.zeros((2,2))
     for k in range(nSteps):
-        # propagte
-        EKF.propagate(dt)
-
+        # log
+        xt[k,:] = xk.copy()
+        xf[k,:] = Enkfx.copy()
+        Pxd[k,0] = Pxx[0,0]
+        Pxd[k,1] = Pxx[1,1]
+        # propagate filter
+        #EnKF.propagate(dt)
+        EnKF.propagateOde(dt)
         # simulate
         y = sp.odeint(stateDerivative,xk,np.array([tsim,tsim+dt]),args=([],) )
         xk = y[-1,:].copy()
@@ -122,13 +138,44 @@ def main(argin='./'):
         # measurement
         ymeas = simMeasurementFunction(xk,tsim)
         # update EKF
-        EKF.update(tsim,ymeas,measFunction,measGradient,Rkin)
+        EnKF.update(ymeas)
+        # get the mean and covariance estimate out
+        Enkfx = np.mean(EnKF.xk,axis=1)
+        Pxx = np.zeros((2,2))
+        for k in range(EnKF.get_N()):
+            Pxx = Pxx + 1.0/(1.0+float(EnKF._N))*np.outer(EnKF.xk[:,k]-Enkfx,EnKF.xk[:,k]-Enkfx)
         # log to file
-        FOUT.write('%f,%f,%f,%f,%f,%f,%f,%f\n' % (tsim,xk[0],xk[1],ymeas[0],EKF.xhat[0],EKF.xhat[1],EKF.Pk[0,0],EKF.Pk[1,1]) )
+        FOUT.write('%f,%f,%f,%f,%f,%f,%f,%f\n' % (tsim,xk[0],xk[1],ymeas[0],Enkfx[0],Enkfx[1],Pxx[0,0],Pxx[1,1]) )
 
     FOUT.close()
+    print('Completed simulation')
+
+    # plot
+    tplot = np.arange(0.0,tfin,dt)
+    fig = plt.figure()
+
+    ax = []
+    for k in range(4):
+        if k < 2:
+            nam = 'x' + str(k+1)
+        else:
+            nam = 'e' + str(k-1)
+        ax.append( fig.add_subplot(2,2,k+1,ylabel=nam) )
+        if k < 2:
+            ax[k].plot(tplot,xt[:,k],'b-')
+            ax[k].plot(tplot,xf[:,k],'r--')
+        elif k < 4:
+            ax[k].plot(tplot,xt[:,k-2]-xf[:,k-2],'b-')
+            ax[k].plot(tplot,3.0*np.sqrt(Pxd[:,k-2]),'r--')
+            ax[k].plot(tplot,-3.0*np.sqrt(Pxd[:,k-2]),'r--')
+
+        ax[k].grid()
+    fig.show()
+
+    raw_input("Return to exit")
+
+    print("Completed test_enky.py")
     return
-    """
 
 if __name__ == '__main__':
     main()